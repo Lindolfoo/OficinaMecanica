08f4ac732ac2f588a1048630786a4e1aacb21e27
Henrique Rodrigues Lindolfo <hrlindolfo08@gmail.com>
2026-09-02 22:10:55 -0300

    feat(veiculo): expõe /api/veiculos

--- src/main/java/br/com/oficina/App.java
+++ src/main/java/br/com/oficina/App.java
@@ -2,6 +2,7 @@ package br.com.oficina;
 
 import br.com.oficina.api.ClienteHandler;
 import br.com.oficina.api.ServicoHandler;
+import br.com.oficina.api.VeiculoHandler;
 import br.com.oficina.config.Config;
 import br.com.oficina.db.Database;
 import br.com.oficina.http.StaticHandler;
@@ -41,6 +42,7 @@ public final class App {
         // 2) Rotas
         HttpServer server = HttpServer.create(new InetSocketAddress(Config.port()), 0);
         server.createContext("/api/clientes", new ClienteHandler());
+        server.createContext("/api/veiculos", new VeiculoHandler());
         server.createContext("/api/servicos", new ServicoHandler());
         server.createContext("/", new StaticHandler());
 
